30744f944e74e8582372746ab9a5251491f8f560
hyejisc <gpwl0937@gmail.com>
2023-06-23 10:23:41 +0900

    라우트 중복 제거

--- app_file.js
+++ app_file.js
@@ -13,33 +13,50 @@ app.get("/topic/new", function (req, res) {
   res.render("new");
 });
 
-app.get("/topic", function (req, res) {
+app.get(["/topic", "/topic/:id"], function (req, res) {
   fs.readdir("data", function (err, files) {
     if (err) {
       console.log(err);
       res.status(500).send("Internal Server Error");
     }
-    res.render("view", { topics: files });
-  });
-});
-
-app.get("/topic/:id", function (req, res) {
-  var id = req.params.id;
-
-  fs.readdir("data", function (err, files) {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Internal Server Error");
+    var id = req.params.id;
+    if (id) {
+      // id 값이 있을 때
+      fs.readFile("data/" + id, "utf8", function (err, data) {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Internal Server Error");
+        }
+        res.render("view", { topics: files, title: id, description: data });
+      });
+    } else {
+      // id 값이 없을때
+      res.render("view", {
+        topics: files,
+        title: "welcome",
+        description: "hello javascript",
+      });
     }
-    fs.readFile("data/" + id, "utf8", function (err, data) {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Internal Server Error");
-      }
-      res.render("view", { topics: files, title: id, description: data });
-    });
   });
 });
+
+// app.get("/topic/:id", function (req, res) {
+//   var id = req.params.id;
+
+//   fs.readdir("data", function (err, files) {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send("Internal Server Error");
+//     }
+//     fs.readFile("data/" + id, "utf8", function (err, data) {
+//       if (err) {
+//         console.log(err);
+//         res.status(500).send("Internal Server Error");
+//       }
+//       res.render("view", { topics: files, title: id, description: data });
+//     });
+//   });
+// });
 
 app.post("/topic", function (req, res) {
   var title = req.body.title;
